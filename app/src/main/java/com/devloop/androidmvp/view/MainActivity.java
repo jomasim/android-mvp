6aac6772e0cf129ba657a6ef39e65790acbe7cbf
jomasim <simjoms@gmail.com>
2019-10-30 13:30:00 +0300

    set up

---
+++ app/src/main/java/com/devloop/androidmvp/view/MainActivity.java
@@ -0,0 +1,23 @@
+package com.devloop.androidmvp.view;
+
+import android.os.Bundle;
+
+import com.devloop.androidmvp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
